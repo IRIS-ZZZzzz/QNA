6e1fccac24b6ebbfd0bb642266e33b36fa705f41
IRISZZZ <coscos0607@gmail.com>
2022-06-02 22:00:57 +0800

    Initial Commit

---
+++ QNA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QNA
+//
+//  Created by 鍾瑀璇 on 2022/6/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
